e008e94e3b8fb9e93785d157c10a70f427850e67
gennadiy123 <shelukhin_g@ukr.net>
2023-04-19 21:11:21 +0300

    cached requests

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -5,41 +5,58 @@ import { getUsers, deleteUser, addUser, updateUser } from "./usersSlice";
 const url = "https://64234be5001cb9fc203ca00b.mockapi.io/users-data";
 
 export const getAllUsers = () => {
-  return (dispatch) => {
-    axios
-      .get(url)
-      .then((response) => response.data)
-      .then((data) => dispatch(getUsers(data)))
-      .catch((error) => console.log("error", error));
+  return async (dispatch) => {
+    try {
+      let data;
+      if (!localStorage.getItem("tableData")) {
+        data = await axios.get(url).then((response) => response.data);
+        localStorage.setItem("tableData", JSON.stringify(data));
+      } else {
+        data = JSON.parse(localStorage.getItem("tableData"));
+      }
+
+      dispatch(getUsers(data));
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 };
 
 export const removeUser = (id) => {
-  return (dispatch) => {
-    axios
-      .delete(`${url}/${id}`)
-      .then((response) => response.data)
-      .then((data) => dispatch(deleteUser(id)))
-      .catch((error) => console.log("error", error));
+  return async (dispatch) => {
+    try {
+      const data = await axios
+        .delete(`${url}/${id}`)
+        .then((response) => response.data);
+      dispatch(deleteUser(data.id));
+      const storage = JSON.parse(localStorage.getItem("tableData"));
+      const filteredStorage = storage.filter((item) => item.id !== data.id);
+      localStorage.setItem("tableData", JSON.stringify(filteredStorage));
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 };
 
 export const addOneUser = ({ name, age, aboutPerson }) => {
-  return (dispatch) => {
-    const id = nanoid();
-    axios({
-      method: "post",
-      url: url,
-      data: {
-        name: `${name}`,
-        age: `${age}`,
-        "about-person": `${aboutPerson}`,
-        id: id,
-      },
-    })
-      .then((response) => response.data)
-      .then((data) => dispatch(addUser(data)))
-      .catch((error) => console.log("error", error));
+  return async (dispatch) => {
+    try {
+      const id = nanoid();
+      const data = await axios({
+        method: "post",
+        url: url,
+        data: {
+          name: `${name}`,
+          age: `${age}`,
+          "about-person": `${aboutPerson}`,
+          id: id,
+        },
+      }).then((response) => response.data);
+      dispatch(addUser(data));
+      const storage = JSON.parse(localStorage.getItem("tableData"));
+      const updatedStorage = storage.concat(data);
+      localStorage.setItem("tableData", JSON.stringify(updatedStorage));
+    } catch (error) {}
   };
 };
 
@@ -49,19 +66,32 @@ export const changeOneUser = ({
   name,
   "about-person": aboutPerson,
 }) => {
-  return (dispatch) => {
-    axios({
-      method: "put",
-      url: `${url}/${id}`,
-      data: {
-        name: name,
-        age: age,
-        "about-person": aboutPerson,
-      },
-    })
-      .then((response) => response.data)
-      .then((data) => dispatch(updateUser(data)))
-      .catch((error) => console.log("error", error));
+  return async (dispatch) => {
+    try {
+      const data = await axios({
+        method: "put",
+        url: `${url}/${id}`,
+        data: {
+          name: name,
+          age: age,
+          "about-person": aboutPerson,
+        },
+      }).then((response) => response.data);
+      dispatch(updateUser(data));
+      const storage = JSON.parse(localStorage.getItem("tableData"));
+      const updatedStorage = storage.map((el) => {
+        if (el.id === data.id) {
+          return Object.assign(el, {
+            name: data.name,
+            age: data.age,
+            "about-person": data["about-person"],
+          });
+        }
+        return el
+      });
+      localStorage.setItem("tableData", JSON.stringify(updatedStorage));
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 };
- 
